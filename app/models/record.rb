79f63c18715eb1591d0a18f3681a0562a94b5076
Marco D <mcruz@geosatelital.com.pe>
2021-09-08 12:14:10 -0500

    Validacion calendario

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,21 +2,13 @@ class Record < ApplicationRecord
   belongs_to :employee
 
   def self.porEmpleado(id)
-    Record.where("employee_id LIKE ?", "#{id}")
+    Record.where("employee_id = #{id}")
   end
 
   def self.reporte(id)
-    Record.joins(:employee).where(employee_id: id).order(:job_entry)
+    Record
+          .joins(:employee)
+          .where("employee_id = #{id}")
+          .order(:job_entry)
   end
-
-  #def self.to_csv(id)
-  #  CSV.generate do |csv|
-  #    csv << column_names
-  #    
-  #    reporte.each do |record|
-  #      csv << record.attributes.values_at(*column_names)
-  #    end
-  #  end
-  #end
-
 end
